7ecd8c844d3b05eb54e391e186e42f182e1a920e
Johnathan Graham <54722920+jgraham8@users.noreply.github.com>
2025-07-04 19:49:06 +0100

    fixed servicediscovery and implemented ci-cd workflow

--- BogCraft.Aspire/AppHost.cs
+++ BogCraft.Aspire/AppHost.cs
@@ -1,9 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Add the UI project as a service
-var ui = builder.AddProject<Projects.BogCraft_UI>("bogcraft-ui")
-    .WithHttpsEndpoint(port: 7019, name: "https")
-    .WithHttpEndpoint(port: 5091, name: "http")
-    .WithExternalHttpEndpoints();
+var ui = builder.AddProject<Projects.BogCraft_UI>("bogcraft-ui");
 
 builder.Build().Run();
